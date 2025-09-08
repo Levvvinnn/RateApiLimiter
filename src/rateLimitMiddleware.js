389d91c1480a96ff43b1e9d84712a098731da095
Levin <levinmathewshibu@gmail.com>
2025-09-08 03:21:10 +0000

    Sep 7, 2025, 8:21 PM

--- src/rateLimitMiddleware.js
+++ src/rateLimitMiddleware.js
@@ -11,7 +11,8 @@ function createRateLimit(capacity,refillRate,refillInterval){
         }else{
             res.status(429).json({
                 error:"Rate limit exceeded",
-                message:"Too many requests"
+                message:"Too many requests",
+                retry:""
             });
 
         }
