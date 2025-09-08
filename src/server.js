66bad8492b2f8f838221a3a8579a07ad227ba15b
Levin <levinmathewshibu@gmail.com>
2025-09-08 01:42:08 +0000

    Sep 7, 2025, 6:42 PM

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ const rateLimiter=createRateLimit(5,1,1000);
 app.use(rateLimiter);
 
 app.get('/',(req,res)=>{
-    res.json({message:})
+    res.json({message:"Hii this endpoint is ratelimited"});
 })
 
