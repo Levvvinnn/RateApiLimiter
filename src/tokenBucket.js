8a4aa96af39c3f2e470f6e4d3caf5a117d19ae67
Levin <levinmathewshibu@gmail.com>
2025-09-08 01:06:17 +0000

    Sep 7, 2025, 6:06 PM

--- src/tokenBucket.js
+++ src/tokenBucket.js
@@ -8,7 +8,9 @@ class tokenBucket{
         this.refillInterval=refillInterval;
         this.buckets=new Map();
 
-        setInterval(()=>)
+        setInterval(()=>{
+            
+        })
     }
     getBucket(clientId){
         if(!this.buckets.has(clientId)){
