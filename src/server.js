fd92c5dc926c9a0c5510422d5abcb0d8e8b40b6e
Levin <levinmathewshibu@gmail.com>
2025-09-08 02:06:50 +0000

    Sep 7, 2025, 7:06 PM

--- src/server.js
+++ src/server.js
@@ -17,6 +17,10 @@ app.get('/',publicLimiter,(req,res)=>{
     res.json({message:"Public limiter-higher rate limit"});
 });
 
+app.get('/',publicLimiter,(req,res)=>{
+    res.json({message:"Public limiter-higher rate limit"});
+});
+
 app.listen(port,()=>{
     console.log(`Server running on http://localhost:${port}`);
 });
